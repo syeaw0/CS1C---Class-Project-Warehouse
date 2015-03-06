28aa65d1574cd46235dd863351c358760e581e8d
syeaw0 <samyeaw@yahoo.com>
2015-03-05 19:37:46 -0800

    Update - Successful Build!
    I fixed up all of the compile errors, so the build is successful.
    However, we still cannot test the outputs because we need to read in the
    information from a file.

---
+++ MemberList.h
@@ -0,0 +1,40 @@
+/**************************************************************************
+ * AUTHOR        : Jacob Mc'Iver
+ * STUDENT ID    : 265933
+ * Assignment #  : Group Project
+ * CLASS         : MW 12:30pm - 4:20pm
+ * SECTION       : CS1C
+ * DUE DATE      : 3//2015
+ **************************************************************************/
+
+#ifndef MEMBERLIST_H_
+#define MEMBERLIST_H_
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "Preferred.h"
+
+using namespace std;
+
+class MemberList
+{
+public:
+
+	/*METHODS*/
+
+	MemberList();
+	~MemberList();
+	void CreateList(ifstream &inFile);
+//	void DeleteMember(string deleteName);
+	Basic *SearchMember(int userId)const;
+	Basic *GetHead()const;
+
+private:
+
+	Basic* head;
+
+};
+
+#endif /* MEMBERLIST_H_ */
---
+++ inventory.cpp
@@ -0,0 +1,130 @@
+/*************************************************************************
+ * AUTHOR     		 : Arman Jafarinia
+ * STUDENT ID 		 : 912488
+ * Assignment #2     : student
+ * CLASS      		 : CS 1C
+ * SECTION    		 : T Th 12:00-1:50
+ * DUE DATE   		 :
+ * ***********************************************************************/
+
+#include "Inventory.h"
+
+Inventory::Inventory()
+{
+	numberOfItems = 0;
+	head = NULL;
+}
+
+Inventory::~Inventory()
+{
+	Item*	perPtr;
+
+	perPtr = head;
+
+	while(perPtr != NULL)
+	{
+		head = head->GetNextItem();
+		delete perPtr;
+		perPtr = head;
+	}
+}
+
+void Inventory::SetNumberOfItems(int amountOfItems)
+{
+	numberOfItems = amountOfItems;
+}
+
+void Inventory::ReadInFile(ifstream &inFile)
+{
+	Item *itemPtr;
+	Date aDate;
+	int memberId;
+	string itemName;
+	float cost;
+	int quantity;
+
+	itemPtr = new Item;
+	while (inFile && itemPtr != NULL)
+	{
+		aDate.SetDate(inFile);
+		itemPtr->SetDatePurchased(aDate);
+		inFile >> memberId;
+		itemPtr->SetPurchaseID(memberId);
+		inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+		getline(inFile, itemName);
+		inFile >> cost;
+		itemPtr->SetItemPrice(cost);
+		inFile >> quantity;
+		itemPtr->SetItemQuantity(quantity);
+		inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		itemPtr->SetNextItem(head);
+		head	= itemPtr;
+		itemPtr	= new Item;
+	}
+
+	delete itemPtr;
+	itemPtr = NULL;
+}
+
+Item *Inventory::SearchItem(int purchaseCode)
+{
+	Item *itemPtr;
+	bool found = false;
+
+	itemPtr = head;
+
+	while (itemPtr != NULL && !found)
+	{
+		if (itemPtr->GetBuyerID() == purchaseCode)
+		{
+			found = true;
+		}
+		else
+		{
+			itemPtr	= itemPtr->GetNextItem();
+		}
+	}
+
+	return itemPtr;
+}
+
+//Overloaded Search w/ string input
+Item *Inventory::SearchItem(string purchaseItemName)
+{
+	Item *itemPtr;
+	bool found = false;
+
+	itemPtr = head;
+
+	while (itemPtr != NULL && !found)
+	{
+		if (itemPtr->GetName() == purchaseItemName)
+		{
+			found = true;
+		}
+		else
+		{
+			itemPtr	= itemPtr->GetNextItem();
+		}
+	}
+
+	return itemPtr;
+}
+
+void Inventory::AddToList(Item *newItem)
+{
+	newItem->SetNextItem(head);
+	head = newItem;
+
+}
+
+int Inventory::GetNumberOfItems()const
+{
+	return numberOfItems;
+}
+
+Item *Inventory::GetHead()const
+{
+	return head;
+}
