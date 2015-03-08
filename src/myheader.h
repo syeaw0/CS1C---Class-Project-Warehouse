9d2bc8383a7e4cdac9daa0201abd89e3cfaf4c72
mciver24 <jacobmciver@yahoo.com>
2015-03-07 17:34:20 -0800

    Create myheader.h

---
+++ src/myheader.h
@@ -0,0 +1,32 @@
+/*
+ * Myheader.h
+ *
+ *  Created on: Mar 7, 2015
+ *      Author: jacob_000
+ */
+
+#ifndef MYHEADER_H_
+#define MYHEADER_H_
+
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const string MENU = "========================================\n"
+					"	Warehouse ProtoType\n"
+					"========================================\n"
+					"0 - Exit\n"
+					"1. Output Sales Report\n"
+					"2. Search For Member Purchases\n"
+					"3. Output Total Purchases\n"
+					"4. Output Total Items Sold\n"
+					"5. Output Prefferred Member Rebates\n"
+					"6. Output All Member Info\n"
+					"7. Output Inventory\n"
+					"8. Output Total Purchases\n";
+
+
+
+
+#endif /* MYHEADER_H_ */
