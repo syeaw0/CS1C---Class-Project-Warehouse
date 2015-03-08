369eff99b39b27a336b3fb413fec496cfa2af119
mciver24 <jacobmciver@yahoo.com>
2015-03-07 17:32:15 -0800

    Update MemberList.cpp

--- src/MemberList.cpp
+++ src/MemberList.cpp
@@ -1,5 +1,3 @@
-
-
 /**************************************************************************
  * AUTHOR        : Jacob Mc'Iver
  * STUDENT ID    : 265933
@@ -9,116 +7,34 @@
  * DUE DATE      : 3//2015
  **************************************************************************/
 
-#include "memberList.h"
-#include <cstdlib>
+#ifndef MEMBERLIST_H_
+#define MEMBERLIST_H_
+
 #include <iostream>
+#include <iomanip>
 #include <fstream>
+#include <string>
+#include "Preferred.h"
 
 using namespace std;
 
-MemberList::MemberList()
+class MemberList
 {
-	head = NULL;
-}
+public:
 
-MemberList::~MemberList()
-{
+	/*METHODS*/
 
-}
+	MemberList();
+	~MemberList();
+	void CreateList(ifstream &inFile);
+//	void DeleteMember(string deleteName);
+	Basic *SearchMember(int userId);
+	Basic *GetHead()const;
 
-void MemberList::CreateList(ifstream &inFile)
-{
-	// VARIABLE DECLARATIONS
-	Basic*	memberPtr;
-	string 	name;
-	int		id;
-	string  memberType;
-	Date	expiration;
+private:
 
+	Basic* head;
 
+};
 
-	memberPtr = new Basic;
-
-	// Loop
-	while(inFile && memberPtr != NULL)
-	{
-		getline(inFile, name);
-		inFile >> id;
-		inFile.ignore(1000, '\n');
-		getline(inFile, memberType);
-		//expiration.SetDate(inFile);
-
-		if(memberType == "Basic")
-		{
-			memberPtr	= new Basic;
-		}
-		else
-		{
-			memberPtr	= new Preferred;
-		}
-
-		if(memberPtr != NULL)
-		{
-			memberPtr->SetName(name);
-			memberPtr->SetId(id);
-			//memberPtr->SetExpiration(expiration);
-			memberPtr->SetNext(head);
-			head	= memberPtr;
-			memberPtr = new Basic;
-		}
-
-
-	}
-	delete memberPtr;
-	memberPtr = NULL;
-}
-
-//	void memberList :: DeleteMember(memberInfo* head)
-//	{
-//
-//		 memberInfo* memberPtr;
-//		 memberPtr = head;
-//
-//		 if (head !=NULL )
-//		 {
-//			 memberPtr = memberPtr -> next;
-//			 head = memberPtr;
-//		 }
-//
-//		delete memberPtr;
-//
-//	}
-
-Basic *MemberList::SearchMember(int userId)
-{
-	Basic *memberPtr;
-	bool found;
-	int y;
-
-	found = false;
-	memberPtr = head;
-	y = memberPtr->GetId();
-	cout  << y;
-
-
-//	for(int i=0; i < 11; i++)
-//	{
-//		if (memberPtr->GetId() == userId)
-//		{
-//			cout << "test " << memberPtr->GetId() << endl;
-//			found = true;
-//		}
-//		else
-//		{
-//			memberPtr = memberPtr->GetNext();
-//		}
-//	}
-
-	return memberPtr;
-}
-
-Basic *MemberList::GetHead() const
-{
-	return head;
-}
-
+#endif /* MEMBERLIST_H_ */
